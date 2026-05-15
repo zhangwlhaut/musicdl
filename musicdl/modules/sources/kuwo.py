c897f56fc2af989f5ada4d9df19fc2dd524e9932
CharlesPikachu <blwx96@connect.hku.hk>
2026-05-15 18:33:34 +0800

    one flac api for kuwo music client

--- musicdl/modules/sources/kuwo.py
+++ musicdl/modules/sources/kuwo.py
@@ -126,6 +126,23 @@ class KuwoMusicClient(BaseMusicClient):
         )
         # return
         return song_info
+    '''_parsewithlxmusicapi'''
+    def _parsewithlxmusicapi(self, search_result: dict, request_overrides: dict = None):
+        # init
+        request_overrides, song_id = request_overrides or {}, str(search_result.get('MUSICRID') or search_result.get('musicrid')).removeprefix('MUSIC_')
+        headers = {'Content-Type': 'application/json', 'User-Agent': 'lx-music-request/2.6.0', 'X-Request-Key': 'share-v3'}
+        if not (search_result.get('SONGNAME') or search_result.get('name') or search_result.get('songName')): search_result.update(self._getsongmetainfo(song_id=song_id, request_overrides=request_overrides))
+        # parse
+        (resp := self.get(f"https://lxmusicapi.onrender.com/url/kw/{song_id}/flac", headers=headers, **request_overrides)).raise_for_status()
+        if not (download_url := (download_result := resp2json(resp=resp))['url']) or not str(download_url).startswith('http'): return SongInfo(source=self.source)
+        duration_in_secs = int(float(search_result.get('DURATION') or search_result.get('duration') or 0))
+        download_url_status: dict = self.audio_link_tester.test(url=download_url, request_overrides=request_overrides, renew_session=True)
+        song_info = SongInfo(
+            raw_data={'search': search_result, 'download': download_result, 'lyric': {}}, source=self.source, song_name=legalizestring(search_result.get('SONGNAME') or search_result.get('name') or search_result.get('songName')), singers=legalizestring(search_result.get('ARTIST') or search_result.get('artist')), album=legalizestring(search_result.get('ALBUM') or search_result.get('album')), ext=download_url_status['ext'], 
+            file_size_bytes=download_url_status['file_size_bytes'], file_size=download_url_status['file_size'], identifier=song_id, duration_s=duration_in_secs, duration=SongInfoUtils.seconds2hms(duration_in_secs), lyric=None, cover_url=search_result.get('hts_MVPIC') or search_result.get('albumpic') or search_result.get('pic'), download_url=download_url_status['download_url'], download_url_status=download_url_status, 
+        )
+        # return
+        return song_info
     '''_parsewithnxinxzapi'''
     def _parsewithnxinxzapi(self, search_result: dict, request_overrides: dict = None):
         # init
@@ -165,7 +182,7 @@ class KuwoMusicClient(BaseMusicClient):
     '''_parsewiththirdpartapis'''
     def _parsewiththirdpartapis(self, search_result: dict, request_overrides: dict = None):
         if self.default_cookies or request_overrides.get('cookies'): return SongInfo(source=self.source)
-        for parser_func in [self._parsewithcggapi, self._parsewithceseetapi, self._parsewithnxinxzapi, self._parsewithhaitangwapi, self._parsewithyyy001api, self._parsewithguyueiapi]:
+        for parser_func in [self._parsewithcggapi, self._parsewithceseetapi, self._parsewithlxmusicapi, self._parsewithnxinxzapi, self._parsewithhaitangwapi, self._parsewithyyy001api, self._parsewithguyueiapi]:
             song_info_flac = SongInfo(source=self.source, raw_data={'search': search_result, 'download': {}, 'lyric': {}})
             with suppress(Exception): song_info_flac = parser_func(search_result, request_overrides)
             if song_info_flac.with_valid_download_url and song_info_flac.ext in AudioLinkTester.VALID_AUDIO_EXTS: break
